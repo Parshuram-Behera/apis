6df500ede11cea6e9e4fca3712cd29bec3998426
Parshuramb7 <parshurambehera7735@gmail.com>
2024-07-24 23:22:58 +0530

    test

--- src/main.py
+++ src/main.py
@@ -28,10 +28,6 @@ def main(context):
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
+
+        "Hello Parshuram Behera"
     )
